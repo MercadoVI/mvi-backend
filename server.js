5579397f59259904e253a2c7f5b9e4a4b77ecba2
Realty Investor <contacto@realtyinvestor.eu>
2025-12-12 10:50:59 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const oauthClient = new OAuth2Client();
 
 // ✅ Client IDs permitidos (Web + iOS)
 const GOOGLE_WEB_CLIENT_ID =
-  process.env.GOOGLE_WEB_CLIENT_ID ||
+  process.env.GOOGLE_CLIENT_ID ||
   '329044944531-opeo9m7r3q4e1uf22lhll0atftfv0qp9.apps.googleusercontent.com';
 
 const GOOGLE_IOS_CLIENT_ID =
